c8046c014d408ab40b92154896ef8bb79545eb35
Kuba Buda <kubabuda@gazeta.pl>
2015-12-15 22:41:42 +0100

    rolled back to pipe to branch, and back again here

--- include/mplayer.h
+++ include/mplayer.h
@@ -1,13 +1,10 @@
 #include  "utils.h"
 #include  "utils.h"
 
-int close_mp3(int *mp3_pid);
+int close_mp3(void);
 
-int play_path(char *path, status_t *st);
+int play_locally(char *path);
 
-int play_locally(char *path, status_t *st);
-
-/* command slave mp3 player */
 void mplayer_pause(void);
 
 void mplayer_louder(void);
--- src/mplayer.c
+++ src/mplayer.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include  "ao/ao.h"
+#include  "curl/curl.h"
 #include  "mpg123.h"
 
 #include  "controls.h"
@@ -17,83 +19,109 @@
 #define COMMAND         "-C"
 
 
-int close_mp3(int *mp3_pid)
+#define BITS            8
+
+
+static char initd;
+static char play;
+static off_t pos;
+char stamp[TIME_BUFLEN];
+
+
+
+int init_mp3(void)
 {
-    int i = -1;
-    if(mp3_pid != 0) {
-        i = kill(*mp3_pid, SIGKILL);
-        mp3_pid = 0;
+    static int i;
+
+    if(!initd) {
+        ao_initialize();
+        i = ao_default_driver_id();
+        mpg123_init();
     }
+    initd = 1;
+    play = 1;
+    pos = 1;
+
     return i;
 }
 
-int play_path(char *path, status_t *st)
+int close_mp3(void)
 {
-    int i;
-    close_mp3(&st->mp3_pid);
+    pos = 0;
+    play = 0;
+    mpg123_exit();
+    ao_shutdown();
 
-    mpg123_init();
-
-    if((st->mp3_pid = fork()) == 0) {
-        // launch mpg123 in separate process binded by two pipes
-        dup2(st->to_music_player[0], STDIN_FILENO);
-        dup2(st->from_music_player[1], STDERR_FILENO);
-
-        printf("[CALLING]%s %s %s\n", PLAYER_PATH, COMMAND, path);
-        i = execlp(PLAYER_PATH, PLAYER_PATH, COMMAND, path, NULL);
-        if(i) {
-            perror("Invalid calling of mp3 player");
-        }
-        return i;
-    } else {
-        if(st->verbose){
-            printf("[%s] Now playing:%s\n", timestamp(st->tmr_buf),
-                   path
-            );
-        }
-        return 0;
-    }
+    return 0;
 }
 
-int play_locally(char *path, status_t *st)
+/* This function is inspired by article of Johnny Huang
+ * http://hzqtc.github.io/2012/05/play-mp3-with-libmpg123-and-libao.html
+ */
+int play_local(char *path)
 {
-    char full_path[COMMAND_MAX_LEN];
-    if (access(full_path, F_OK) != -1) {
-        play_path(full_path, st);
+    ao_device *dev;
+    ao_sample_format format;
+    mpg123_handle *mh;
+    unsigned char *buffer;
+    int driver;
+    int err;
+    int channels, encoding;
+    long rate;
+    size_t buffer_size;
+    size_t done;
+
+    driver = init_mp3();
+
+    mh = mpg123_new(NULL, &err);
+    buffer_size = mpg123_outblock(mh);
+    buffer = malloc(buffer_size * sizeof(unsigned char));
+
+    mpg123_open(mh, path);
+    mpg123_getformat(mh, &rate, &channels, &encoding);
+
+    format.bits = mpg123_encsize(encoding) * BITS;
+    format.rate = rate;
+    format.channels = channels;
+    format.byte_format = AO_FMT_NATIVE;
+    format.matrix = 0;
+    dev = ao_open_live(driver, &format, NULL);
+
+    while(mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK && play) {
+        ao_play(dev, (char*)buffer, done);
+    }
+    free(buffer);
+    ao_close(dev);
+    mpg123_close(mh);
+    mpg123_delete(mh);
+//    mpg123_exit();
+//    ao_shutdown();
+
+    return err;
+}
+
+
+int play_locally(char *path)
+{
+    if (access(path, F_OK) != -1) {
+        printf("[%s] Now playing: %s\n", timestamp(stamp), path);
+        play_local(path);
     } else {
         //file doesn't exist, something went wrong
+        return -1;
     }
     return 0;
 }
 
-/* write string to slave music player through pipe */
-int swrite_mplayer(int pfd, char *s, int n)
-{
-    int i = 0;
-//    i = write(st->to_music_player[1], s, n);
 
-    return i;
-}
-
-/* write string to slave music player through pipe */
-int cwrite_mplayer(char c)
-{
-    int i = 0;
-
-    i = write(/* *st->to_music_player[1]*/2, &c, 1);
-
-    return i;
-}
-
-/* command slave mp3 player */
 void mplayer_pause(void)
 {
-    cwrite_mplayer(MPG123_PAUSE_KEY);
+
 }
 
 void mplayer_louder(void)
 {
-    cwrite_mplayer(MPG123_VOL_UP_KEY);
+
 }
 
 
