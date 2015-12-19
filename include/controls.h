1ce53ba855240cf64ce1647d935b00078111c938
Kuba Buda <kubabuda@gazeta.pl>
2015-12-19 23:39:15 +0100

    Still using detached client threads with messy exit

--- include/controls.h
+++ include/controls.h
@@ -5,16 +5,19 @@
 #define MPLAYER_MODE_SET 's'
 #define MPLAYER_MODE_LIST 'l'
 #define MPLAYER_MODE_PLAY 'p'
+#define MPLAYER_MODE_EXIT 'e'
 
 #define MPLAYER_PLAY_PODCAST 'p'
 #define MPLAYER_PLAY_LOCAL  'l'
-
 #define MPLAYER_SET_PAUSE 'p'
 #define MPLAYER_SET_STOP 's'
 #define MPLAYER_SET_VOL_UP '+'
 #define MPLAYER_SET_VOL_DOWN '-'
 #define MPLAYER_SET_FORWARD '>'
 #define MPLAYER_SET_REWIND '<'
+
+typedef enum player_cmd_t {PLAY_LOCAL, PAUSE, STOP, REWIND, FORWARD, VOL_UP,
+                            VOL_DOWN, EXIT} player_cmd_t;
 
 /* Commands for backends */
 /* MPG123, straight from its source  */
